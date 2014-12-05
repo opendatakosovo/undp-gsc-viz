a2de349ec8083bb8a43ccb24e72b36d08f115217
Georges Labreche <georges@opendatakosovo.org>
2014-12-05 02:29:12 -0800

    use same template for all questions

--- ugcv/views/pages/answers.py
+++ ugcv/views/pages/answers.py
@@ -4,9 +4,6 @@ from flask.views import View
 class Answers(View):
     def dispatch_request(self, qid):
 
-    	if qid in [1, 2, 5, 7, 10, 11, 12, 13]:
-        	return render_template('multichoice.html', qid=qid)
-        else:
-        	return "other"
+        return render_template('multichoice.html', qid=qid)
 
         
