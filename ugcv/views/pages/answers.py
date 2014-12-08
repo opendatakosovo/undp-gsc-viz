08316ca75b9e2e90c8ddead6acc38c9299b2f457
Vullkan92 <vullkan.halili@riinvest.net>
2014-12-08 15:12:02 +0100

    Added barchart to question 4,7 and 14

--- ugcv/views/pages/answers.py
+++ ugcv/views/pages/answers.py
@@ -3,7 +3,7 @@ from flask.views import View
 
 class Answers(View):
     def dispatch_request(self, qid):
-
-        return render_template('multichoice.html', qid=qid)
-
-        
+        if qid in [4, 7, 14]:
+            return render_template('barchart.html', qid=qid)
+        else:
+            return render_template('multichoice.html', qid=qid)
